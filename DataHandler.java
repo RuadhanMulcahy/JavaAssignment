d608312a1af283bfe4a206c7ace69fa27b441795
RuadhanMulcahy <36204464+RuadhanMulcahy@users.noreply.github.com>
2018-04-11 16:03:28 +0100

    Add files via upload

--- DataHandler.java
+++ DataHandler.java
@@ -1,6 +1,7 @@
 package assignment1;
 
 import java.sql.*;
+import java.util.*;
 
 public class DataHandler {
 	
@@ -12,8 +13,6 @@ public class DataHandler {
 	
 	int i = 0;
 	
-	String results[] = {};
-	
 	public DataHandler(String val1, String val2, String val3, String val4, String val5) {
 		
 		this.val1 = val1;
@@ -23,10 +22,13 @@ public class DataHandler {
 		this.val5 = val5;
 	}
 	
+	public DataHandler() {
+		
+	}
+	
 	public void mainDriver() {	
 		
 		try {
-			
 			
 			Connection myConn = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xe", "system", "student");
 			
@@ -34,11 +36,14 @@ public class DataHandler {
 			
 			ResultSet stat1 = myStmt.executeQuery("select count(" + val1 + ") from FIRESTAT where " + val1 +  " = '" + val2 + "' and " + val3 + " between '" + val4 + "' and '" + val5 + "'");
 			//ResultSet stat1 = myStmt.executeQuery("select unique(Description) from FIRESTAT");
+			//ResultSet stat1 = myStmt.executeQuery("count(" + val1 + ")");
 			
 			while (stat1.next()) {
 				
 				//System.out.println(stat1.getString("Description") + " | " + stat1.getString("DATES") + ("|") + stat1.getString("STATIONAREA"));
+				//System.out.println(stat1.getString("count(" + val1 + ")"));
 				System.out.println(stat1.getString("count(" + val1 + ")"));
+				
 			}
 			
 		}
@@ -46,5 +51,30 @@ public class DataHandler {
 			
 			exc.printStackTrace();
 		}
+	}
+	
+	public List<String> retrieveVars(String value) {
+		
+		List<String> list = new ArrayList<String>();
+		
+		try {
+			
+			Connection myConn = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xe", "system", "student");
+			
+			Statement myStmt = myConn.createStatement();
+			
+			ResultSet stat1 = myStmt.executeQuery("select unique(" + value + ") from FIRESTAT");
+			
+			while (stat1.next()) {
+				
+				list.add(stat1.getString(value));
+			}
+		}
+		catch (Exception exc) {
+			
+			exc.printStackTrace();
+		}
+		
+		return list;
 	}
 }
--- SidePanel.java
+++ SidePanel.java
@@ -4,27 +4,36 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.*;
 
 public class SidePanel  implements ActionListener {
-	 
-	//try extending display or graph
 	
 	String[] options = {"", "1", "2", "3", "4", "5"};
+	String[] columnNames1 = {"","Station Area", "Description"};
+	String[] columnNames2 = {"Dates", "TOC", "ORD", "MOB", "IA", "MAV", "CD"};   
 	String[] type = {"", "Pie Chart", "Graph"};
 	
-	JComboBox dropDown1 = new JComboBox(options);
-	JComboBox dropDown2 = new JComboBox(options);
-	JComboBox dropDown3 = new JComboBox(options);
-	JComboBox dropDown4 = new JComboBox(options);
-	JComboBox dropDown5 = new JComboBox(type);
+	JComboBox dropDown1 = new JComboBox(columnNames1);
+	JComboBox dropDown2 = new JComboBox();
+	JComboBox dropDown3 = new JComboBox();
+	JComboBox dropDown4 = new JComboBox();
+	JComboBox dropDown5 = new JComboBox();
+	JComboBox dropDown6 = new JComboBox(type);
 	
 	JLabel blank1 = new JLabel("");
 	JLabel blank2 = new JLabel("");
 	JLabel blank3 = new JLabel("");
 	JLabel blank4 = new JLabel("");
 	JLabel blank5 = new JLabel("");
+	JLabel blank6 = new JLabel("");
+	JLabel blank7 = new JLabel("");
+	
+	JButton button1 = new JButton("View");
+	
+	String selection = "";
 	
 	JPanel container = new JPanel();
 	JFrame window = new JFrame();
@@ -50,30 +59,25 @@ public class SidePanel  implements ActionListener {
 		sidePanel.setPreferredSize(new Dimension(100,200));
 		sidePanel.setLayout(new BoxLayout(sidePanel, BoxLayout.Y_AXIS));
 		
-		dropDown1.setPreferredSize(new Dimension(100,20));
 		dropDown1.setMaximumSize(new Dimension(100,20));
-		dropDown2.setPreferredSize(new Dimension(100,20));
 		dropDown2.setMaximumSize(new Dimension(100,20));
-		dropDown3.setPreferredSize(new Dimension(100,20));
 		dropDown3.setMaximumSize(new Dimension(100,20));
-		dropDown4.setPreferredSize(new Dimension(100,20));
 		dropDown4.setMaximumSize(new Dimension(100,20));
-		dropDown5.setPreferredSize(new Dimension(100,20));
 		dropDown5.setMaximumSize(new Dimension(100,20));
+		dropDown6.setMaximumSize(new Dimension(100, 20));
 		
-		blank1.setPreferredSize(new Dimension(100,20));
 		blank1.setMaximumSize(new Dimension(100,20));
-		blank2.setPreferredSize(new Dimension(100,20));
 		blank2.setMaximumSize(new Dimension(100,20));
-		blank3.setPreferredSize(new Dimension(100,20));
 		blank3.setMaximumSize(new Dimension(100,20));
-		blank4.setPreferredSize(new Dimension(100,20));
 		blank4.setMaximumSize(new Dimension(100,20));
-		blank5.setPreferredSize(new Dimension(100,20));
 		blank5.setMaximumSize(new Dimension(100,20));
+		blank6.setMaximumSize(new Dimension(100, 20));
+		blank7.setMaximumSize(new Dimension(100, 20));
 		
 		dropDown1.addActionListener(this);
-		dropDown5.addActionListener(this);
+		dropDown2.addActionListener(this);
+		dropDown6.addActionListener(this);
+		button1.addActionListener(this);
 		
 		sidePanel.add(blank1);
 		sidePanel.add(dropDown1);
@@ -85,53 +89,89 @@ public class SidePanel  implements ActionListener {
 		sidePanel.add(dropDown4);
 		sidePanel.add(blank5);
 		sidePanel.add(dropDown5);
+		sidePanel.add(blank6);
+		sidePanel.add(dropDown6);
+		sidePanel.add(blank7);
+		sidePanel.add(button1);
 		
 		data.mainDriver();
 		
 		return sidePanel;
 	}
 
-	public void actionPerformed(ActionEvent dropDown) {
+	public void actionPerformed(ActionEvent actions) {
 		
-		
-		if(dropDown.getSource() == dropDown1) {
+		if(actions.getSource() == dropDown1) {
+
 			
 			String check = (String) dropDown1.getSelectedItem();
 		
 			switch (check) {
 			
-				case "1":
-					System.out.println("1");
+				case "Station Area":
+					this.retrieval(dropDown2, "StationArea");
 					break;
-				case "2":
-					System.out.println("2");
-					break;
-				case "3":
-					System.out.println("3");
-					break;
-				case "4":
-					System.out.println("4");
+				case "Description":
+					this.retrieval(dropDown2, "Description");
 					break;
 			}
 		}
-		else if(dropDown.getSource() == dropDown5) {
+		else if(actions.getSource() == dropDown2) {
 			
-			String check2 = (String) dropDown5.getSelectedItem();
+			dropDown3.setModel(new DefaultComboBoxModel(columnNames2));
+		}
+		else if(actions.getSource() == dropDown3) {
+			
+		}
+		else if(actions.getSource() == dropDown4) {
+			
+		}
+		else if(actions.getSource() == dropDown5) {
+			
+		}
+		else if(actions.getSource() == dropDown6) {
+			
+			String check2 = (String) dropDown6.getSelectedItem();
 			
 			switch (check2) {
 				
 				case "Pie Chart":
-					Graph demo = new Graph("chart" );  
-				    demo.setSize(400, 300 );  
-				    container.add(demo.createDemoPanel());
-				    window.repaint();
-				    window.setVisible(true);
+					
+					selection = "Pie Chart";
 					break;
 				case "Graph":
 					System.out.println("Graph");
 					break; 
 			}
 		}
+		else if(actions.getSource() == button1) {
+			
+			if(selection == "Pie Chart") {
+				
+				Graph demo = new Graph("chart" );  
+			    demo.setSize(400, 300 );  
+			    container.add(demo.createDemoPanel());
+			    window.repaint();
+			    window.setVisible(true);
+			}
+			else
+			{
+				System.out.println("Histogram");
+			}
+			
+		}
+	}
+	
+	public void retrieval(JComboBox box, String var) {
+		
+		DataHandler retrieve = new DataHandler();
+		
+		List<String> list = retrieve.retrieveVars(var);
+		
+		String[] conversion = new String[list.size()];
+		conversion = list.toArray(conversion);
+		
+		box.setModel(new DefaultComboBoxModel(conversion));
 	}
 }
 
